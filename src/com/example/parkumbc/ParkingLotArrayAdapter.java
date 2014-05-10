b8e9637f5bbf2eee32e9affb8a8b3faab92ed153
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-05-10 00:32:18 -0400

    Highlighting the selected parking lots

--- src/com/example/parkumbc/ParkingLotArrayAdapter.java
+++ src/com/example/parkumbc/ParkingLotArrayAdapter.java
@@ -1,6 +1,8 @@
 package com.example.parkumbc;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,9 @@ import android.widget.TextView;
 import model.ParkingLot;
 
 import java.util.ArrayList;
+import java.util.Set;
+
+import static com.example.parkumbc.Constant.PARKING_LOTS;
 
 public class ParkingLotArrayAdapter extends ArrayAdapter<ParkingLot> {
 
@@ -26,6 +31,12 @@ public class ParkingLotArrayAdapter extends ArrayAdapter<ParkingLot> {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.parking_lot_row_layout, parent, false);
         TextView textViewPermitName = (TextView) rowView.findViewById(R.id.textViewParkingLotName);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        Set<String> lots = preferences.getStringSet(PARKING_LOTS, null);
+        if (lots != null && lots.contains(values.get(position).getLotName()))
+            rowView.setBackgroundColor(0xFFA2D1E9);
+
         textViewPermitName.setText(values.get(position).getLotName());
         return rowView;
     }
