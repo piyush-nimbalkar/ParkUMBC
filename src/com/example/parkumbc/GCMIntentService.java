5af49765b60f29a43efcd9ef057e0076319f00ea
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-05-10 18:08:59 -0400

    Displaying a notification.

--- src/com/example/parkumbc/GCMIntentService.java
+++ src/com/example/parkumbc/GCMIntentService.java
@@ -1,14 +1,19 @@
 package com.example.parkumbc;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import com.google.android.gcm.GCMBaseIntentService;
 import storage.ServerResponse;
 
-import static com.example.parkumbc.Constant.PARKING_LOT_ID;
-import static com.example.parkumbc.Constant.PARKING_LOT_NAME;
-import static com.example.parkumbc.Constant.SENDER_ID;
+import java.util.Set;
+
+import static com.example.parkumbc.Constant.*;
 
 public class GCMIntentService extends GCMBaseIntentService {
 
@@ -32,13 +37,45 @@ public class GCMIntentService extends GCMBaseIntentService {
 
     @Override
     protected void onMessage(Context context, Intent intent) {
-        Log.d(TAG, intent.getExtras().getString(PARKING_LOT_ID));
-        Log.d(TAG, intent.getExtras().getString(PARKING_LOT_NAME));
+        String parkingLotId = intent.getExtras().getString(PARKING_LOT_ID);
+        String parkingLotName = intent.getExtras().getString(PARKING_LOT_NAME);
+        generateNotification(context, parkingLotId, parkingLotName);
     }
 
     @Override
     protected void onError(Context context, String s) {
 
+    }
+
+    private void generateNotification(Context context, String lotId, String lotName) {
+        String message = "No parking spaces left in " + lotName;
+        String tickerText = lotName + " is full!";
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        Set<String> selectedLots = preferences.getStringSet(PARKING_LOTS, null);
+        if (selectedLots != null) {
+            int icon = R.drawable.ic_launcher;
+            long when = System.currentTimeMillis();
+
+            Intent notificationIntent = new Intent(context, MainActivity.class);
+            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+            Notification notification = new Notification.Builder(context)
+                    .setContentTitle(context.getString(R.string.app_name))
+                    .setContentText(message)
+                    .setSmallIcon(icon)
+                    .setWhen(when)
+                    .setTicker(tickerText)
+                    .setContentIntent(pendingIntent)
+                    .build();
+
+            NotificationManager notificationManager = (NotificationManager)
+                    context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notification.flags |= Notification.FLAG_AUTO_CANCEL;
+            notificationManager.notify(0, notification);
+        }
     }
 
 }
